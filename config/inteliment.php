daeb5a7b7dbf3b9977de77800105318f427c94fa
Chris Reed <chris@reedtech.us>
2023-11-26 14:19:33 -0600

    Installed Blade Font Awesome (w/ pro)

---
+++ config/inteliment.php
@@ -0,0 +1,16 @@
+<?php
+
+// config for ChrisReedIO/Inteliment
+return [
+    // 'resources' => [
+    //     'navigation' => [
+    //         'group' => 'OpenAI',
+    //     ],
+    // ],
+    'models' => [
+        'user' => '\App\Models\User',
+    ],
+
+    // Enable the following to use Font Awesome icons
+    'fontawesome' => true,
+];
